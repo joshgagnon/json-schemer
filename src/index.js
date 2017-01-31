6bf23dd1c0626de4b14439c9eb1d5fb4d8cb2789
Paddy Moran <pdmoran@outlook.com>
2017-01-31 15:40:56 +1300

    Fixed shuffle keys

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import isPlainObject from 'lodash/isPlainObject';
+import getKey from './getKey';
+import deepmerge from 'deepmerge';
+import getDefaultValues from './getDefaultValues';
+
 export getDefaultValues from './getDefaultValues';
 export getFields from './getFields';
 export getKey from './getKey';
@@ -5,3 +10,29 @@ export getValidate from './getValidate';
 export injectContext from './injectContext';
 export resolveReferences from './resolveReferences';
 export * from './utils';
+
+
+const arrayMerge = (destinationArray, sourceArray, options) => {
+    return sourceArray;
+}
+
+export function setDefaults(templateSchema, context, values) {
+    const merged = deepmerge(getDefaultValues(templateSchema, context), values, { arrayMerge: arrayMerge });
+    return addKeys(merged);
+}
+
+function addKeys(obj){
+    const recurse = (obj, arrayChild) => {
+        if(Array.isArray(obj)){
+            obj.map((obj) => recurse(obj, true));
+        }
+        else if(isPlainObject(obj)){
+            if (arrayChild) {
+                obj._keyIndex = getKey();
+            }
+            Object.keys(obj).map(k => recurse(obj[k]))
+        }
+        return obj;
+    }
+    return recurse(obj);
+}
