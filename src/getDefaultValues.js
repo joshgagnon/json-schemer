dcc4b7a542b4825147620a73348f17a45cd9b411
Paddy Moran <pdmoran@outlook.com>
2017-01-31 15:08:58 +1300

    It's alive (well at least the array population)

--- src/getDefaultValues.js
+++ src/getDefaultValues.js
@@ -1,28 +1,33 @@
 import getKey from './getKey';
+import { defaultSource } from './utils';
 
 // Appears to not be populating default on list items
-export default function getDefaultValues(schema, defaults={}) {
-    function loop(props, fields, suppliedDefaults) {
+export default function getDefaultValues(schema, context={}) {
+    function loop(props, fields) {
         Object.keys(props).map(key => {
-            if (suppliedDefaults[key]) {
-                fields[key] = suppliedDefaults[key];
-            } else if (props[key].default) {
+            if (defaultSource(props[key]) && context[defaultSource(props[key])]) {
+                // debugger
+                // const source = inputSource(schemaProperties);
+                // fields[key][source] = context[defaultSource(schemaProperties)].map(f => f[source]);
+                props[key].default = context[defaultSource(props[key])];
+            }
+            
+            if (props[key].default) {
                 fields[key] = props[key].default;
             }
-
-            if (props[key].type === 'object') {
+            else if (props[key].type === 'object') {
                 let obj = fields[key] || {};
-                loop(props[key].properties, obj, suppliedDefaults[key] || {});
+                loop(props[key].properties, obj);
                 fields[key] = obj;
             } else if (props[key].type === 'array') {
                 if (props[key].items.type === "object") {
                     let obj = fields[key] || [];
-
-                    loop(props[key].items.properties, obj, {...(suppliedDefaults[key] || {}), _keyIndex: getKey()});
+                    // TODO: add keyIndex default
+                    loop(props[key].items.properties, obj);
                     
                     if (props[key].items.oneOf) {
                         obj.map(o => props[key].items.oneOf.map(oneOf => {
-                            loop(oneOf.properties, o, suppliedDefaults[key] || {});
+                            loop(oneOf.properties, o);
                         }));
                     }
                     
@@ -33,7 +38,7 @@ export default function getDefaultValues(schema, defaults={}) {
             if (props[key].oneOf) {
                 let obj = fields[key] || {};
                 props[key].oneOf.map(o => {
-                    loop(o.properties, obj, suppliedDefaults[key] || {});
+                    loop(o.properties, obj);
                 });
                 fields[key]  = obj;
             }
@@ -42,5 +47,8 @@ export default function getDefaultValues(schema, defaults={}) {
     }
 
     const fields = {};
-    return loop(schema.properties, fields, defaults);
+    const vals = loop(schema.properties, fields);
+    console.log('dadada');
+    console.log(vals);
+    return vals;
 }
