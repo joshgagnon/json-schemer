8989ae460b7a485920d781b07947aa1ec8f01ad6
Paddy Moran <pdmoran@outlook.com>
2017-02-15 14:36:18 +1300

    Allow for multiple select source from the sam source and when one changes, they all reflect that change

--- src/injectContext.js
+++ src/injectContext.js
@@ -5,32 +5,49 @@ export default function injectContext(FormComponent) {
     const Injector = (props) => {
         const fields = injectContext(props.schema.properties, props.fields, props.context);
         return <FormComponent {...props} fields={fields} />;
-    }
+    };
 
     function interceptChangesAndInject(schemaProperties, key, fields, context) {
         if (inputSelectSource(schemaProperties) && fields[key]) {
-            const source = inputSource(schemaProperties);
-            const onChange = fields[key][source].onChange;
+            // Get the source
+            let source = inputSource(schemaProperties);
 
-            fields[key][source].onChange = (event) => {
-                onChange(event);
+            // Source will either be an array of objects with field and property, or a
+            // string that is both the field and property. To make things simpler, if
+            // the source is a string, convert it to the array of objects format
+            if (!Array.isArray(source)) {
+                source = [{
+                    field: source,
+                    property: source
+                }];
+            }
 
-                const contextEventMatch = context[inputSelectSource(schemaProperties)].find(f => f[source] === event);
-
-                if (contextEventMatch) {
-                    const { _keyIndex, ...result } = contextEventMatch;
-
-                    result && Object.keys(result).map(k => {
-                        if (k !== source && fields[key][k]) {
-                            fields[key][k].onChange(result[k]);
-                        }
-                    });
+            source.map((sourceItem) => {
+                // Save the original onChange as _originalOnChange, so we can call it later.
+                // Only do this is the original onChange isn't set (otherwise we'll be saving)
+                // a non-original onChange
+                if (fields[key][sourceItem.field]._originalOnChange === undefined) {
+                    fields[key][sourceItem.field]._originalOnChange = fields[key][sourceItem.field].onChange;
                 }
-            }
 
-            if (context[inputSelectSource(schemaProperties)]) {
-                fields[key][source].comboData = context[inputSelectSource(schemaProperties)].map(f => f[source]);
-            }
+                fields[key][sourceItem.field].onChange = (newValue) => {
+                    // Take the string the user selected and get the object in context it belongs to
+                    const selectedObject = context[inputSelectSource(schemaProperties)].find(f => f[sourceItem.property] === newValue);
+
+                    if (selectedObject) {
+                        Object.keys(selectedObject).map(selectedObjectKey => {
+                            if (selectedObjectKey !== '_keyIndex' && fields[key][selectedObjectKey]) {
+                                fields[key][selectedObjectKey]._originalOnChange(selectedObject[selectedObjectKey]);
+                            }
+                        });
+                    }
+                };
+
+                // If the source exists in context: add that item of context as the comboData.
+                if (context[inputSelectSource(schemaProperties)]) {
+                    fields[key][sourceItem.field].comboData = context[inputSelectSource(schemaProperties)].map(f => f[sourceItem.property]);
+                }
+            });
         }
     }
 
@@ -44,25 +61,33 @@ export default function injectContext(FormComponent) {
                             loop(oneOf.properties, fields[key]);
                         });
                     }
-                } else if (schemaProperties[key].type === 'array') {
+                }
+                else if (schemaProperties[key].type === 'array') {
                     if (schemaProperties[key].items.type === "object") {
                         fields[key] && fields[key].map(f => {
                             loop(schemaProperties[key].items.properties, f);
-                        })
+                        });
+
                         fields[key] && fields[key].map((field, index) => {
                             interceptChangesAndInject(schemaProperties[key].items,  index, fields[key], context);
                         });
+
                         if(schemaProperties[key].items.oneOf) {
                             fields[key].map(f => {
-                                let values = Object.keys(f).reduce((acc, k) => { acc[k] = f[k].value; return acc;}, {});
+                                let values = Object.keys(f).reduce((acc, k) => {
+                                    acc[k] = f[k].value; return acc;
+                                }, {});
+
                                 let result = oneOfMatchingSchema(schemaProperties[key].items, values);
-                                if(result){
+
+                                if (result) {
                                      loop(result.properties, f);
                                 }
                             });
                         }
                     }
                 }
+
                 interceptChangesAndInject(schemaProperties[key], key, fields, context)
             });
 
